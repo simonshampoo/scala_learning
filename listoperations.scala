35e3e9020933123bdac884bc523528ac0481f625
Simon Shamoon <simonshm5@gmail.com>
2022-12-28 01:08:43 -0600

    datboyfa

--- listoperations.scala
+++ listoperations.scala
@@ -1,20 +1,16 @@
 def transform(xs: List[Int]): List[String] = {
   xs match {
-    case Nil => {
-      println("found nil") 
+    case Nil => 
        Nil
-    } 
-    case y::ys => {
-        println(s"calling transform on $y") 
-        "%02d".format(y) :: transform(ys)
-    }
+    case y::ys => 
+        ("%02d".format(y)) :: transform(ys)
   }
 }
 
-
-def foreach(xs: List[Int], f:Int => Int) : Unit = {
+// apply a function on each member of a List
+def foreach [X](xs: List[X], f:X => Unit ): Unit = {
   xs match {
-    case Nil => ()
+    case Nil => Nil 
     case y::ys => {
       f(y)
       foreach(ys, f)
@@ -22,10 +18,37 @@ def foreach(xs: List[Int], f:Int => Int) : Unit = {
   }
 }
 
+def map [X, Y](xs: List[X], f:X=>Y): List[Y] = {
+   xs match {
+    case Nil => Nil 
+    case y::ys => 
+      f(y) :: map (ys, f)
+  }
+}
+
+
 var l = List(1, 2, 3, 4, 5)
-var consList = 1 :: (2 :: (3 :: Nil))
+
+println("Testing transform function")
+println(l)
+transform(l)
+println(l)
+println(l(0).getClass)
 
 
-println(consList)
-transform(consList)
-println(consList)
+println()
+
+
+println("Testing foreach function")
+println(l)
+foreach(l, (x: Int) => println(x))
+println(l)
+
+
+println()
+
+
+println("Testing map function")
+println(l)
+val newL = map(l, (x: Int) => x + 1)
+println(newL)
